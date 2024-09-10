44befb26018df05274a93c4e4e452d63149af4ea
jrme421 <jeromepchallet@gmail.com>
2024-09-10 12:07:18 +0900

    router

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,23 @@
+import Header from "./Header";
+import Loader from "./Loader";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+      <Header />
+      <main>
+        <h1>Content</h1>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
